d4a12f59f6ab220b14147a7a11e7bc4348afebcc
sara-molina <sara.molina.delgado@gmail.com>
2020-05-10 13:34:36 +0200

    Solución al problema de cuando no funciona el login

--- model/profile.php
+++ model/profile.php
@@ -171,7 +171,7 @@ if($GLOBALS['conn']){
             if ($array['contar'] == 0 ) {
                 echo "<div class=\"info-p-div\">
                 <p class =\"info-p\">No estás registrado o tus datos son incorrectos</p>";
-                echo $_SESSION['returnToProfile'];
+                echo "<a class=\"btn btn-full\" href=\"login.html\">Volver</a>";
                 echo "</div>";
             }
     }  
